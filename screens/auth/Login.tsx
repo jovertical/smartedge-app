06476c52e3de88a4fc127d54f6dc9c1dddc98fae
Jovert Palonpon <palonponjovertlota@gmail.com>
2020-02-09 00:56:30 +0800

    chore: Setup prettier

--- screens/auth/Login.tsx
+++ screens/auth/Login.tsx
@@ -1,13 +1,10 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
-import { useScreens } from 'react-native-screens'
-
-useScreens()
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Login Page</Text>
     </View>
   )
 }
